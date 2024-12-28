96434569079bececc91953c47c076a411002f95b
nbanyard <nbanyard@users.noreply.github.com>
2024-12-28 11:17:45 +0000

    Add Rust vim tools

--- .vimrc
+++ .vimrc
@@ -1,1 +1,6 @@
 map gn :NERDTree<cr>
+
+syntax enable
+filetype plugin indent on
+
+au BufWritePre *.rs RustFmt
--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ SCRIPTDIR=$(cd $(dirname "$0"); pwd)
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 
+# Install rust.vim
+git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
+
 # Copy files
 cp "$SCRIPTDIR/.vimrc" ~
